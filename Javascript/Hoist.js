bfe75b3a8eab5bced4db51b044cfa4bf209f1e35
John Barrett <johnjbarrett22@gmail.com>
2020-10-09 13:52:53 -0700

    Corrected previously answered questions and completed Hoisting assignment

--- Javascript/Hoist.js
+++ Javascript/Hoist.js
@@ -19,6 +19,8 @@ let hello;
 console.log(hello);
 hello = 'world';
 
+
+
 //2.
 //Given:
 var needle = 'haystack';
@@ -30,19 +32,21 @@ function test(){
 
 //After hoisting by the interpreter:
 var needle = 'haystack';
-test();
 function test(){
     var needle = 'magnet';
     console.log(needle);
 }
+test();
 
 //Hoisting with ES6:
 let needle = 'haystack';
-test();
 function test(){
     let needle = 'magnet';
     console.log(needle);
 }
+test();
+
+
 
 //3.
 //Given:
@@ -62,12 +66,14 @@ function print(){
 console.log(brendan);
 
 //Hoisting with ES6:
-var brendan = 'super cool';
+let brendan = 'super cool';
 function print(){
     brendan = 'only okay';
     console.log(brendan);
 }
 console.log(brendan);
+
+
 
 //4.
 //Given:
@@ -82,24 +88,27 @@ function eat(){
 
 //After hoisting by the interpreter:
 var food = 'chicken';
-eat();
-console.log(food);
 function eat(){
-    var food = 'gone';
+    var food;
     food = 'half-chicken';
     console.log(food);
+    food = 'gone';
 }
+console.log(food);
+eat();
 
 //Hoisting with ES6:
-mean();
 let food = 'chicken';
-eat();
-console.log(food);
 function eat(){
-    let food = 'gone';
+    let food;
     food = 'half-chicken';
     console.log(food);
+    food = 'gone';
 }
+console.log(food);
+eat();
+
+
 
 //5.
 //Given:
@@ -114,26 +123,31 @@ var mean = function() {
 console.log(food);
 
 //After hoisting by the interpreter:
+var mean;
 mean();
 console.log(food);
-var mean = function() {
-    var food = "fish";
+mean = function() {
+    var food;
     food = "chicken";
     console.log(food);
+    food = "fish";
     console.log(food);
 }
 console.log(food);
 
 //Hoisting with ES6:
+let mean;
 mean();
 console.log(food);
-let mean = function() {
-    let food = "fish";
+mean = function() {
+    let food;
     food = "chicken";
     console.log(food);
+    food = "fish";
     console.log(food);
 }
 console.log(food);
+
 
 
 //6.
@@ -152,12 +166,117 @@ console.log(genre);
 //After hoisting by the interpreter:
 var genre;
 function rewind() {
+    var genre;
     genre = "rock";
     console.log(genre);
-    var genre = "r&b";
+    genre = "r&b";
     console.log(genre);
 }
 console.log(genre);
 genre = "disco";
 rewind();
 console.log(genre);
+
+//Hoisting with ES6:
+let genre;
+function rewind() {
+    let genre;
+    genre = "rock";
+    console.log(genre);
+    genre = "r&b";
+    console.log(genre);
+}
+console.log(genre);
+genre = "disco";
+rewind();
+console.log(genre);
+
+
+
+//7.
+//Given:
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+
+//After hoisting by the interpreter:
+function learn() {
+    var dojo;
+    dojo = "seattle";
+    console.log(dojo);
+    dojo = "burbank";
+    console.log(dojo);
+}
+dojo = "san jose";
+console.log(dojo);
+learn();
+console.log(dojo);
+
+//Hoisting with ES6:
+function learn() {
+    let dojo;
+    dojo = "seattle";
+    console.log(dojo);
+    dojo = "burbank";
+    console.log(dojo);
+}
+dojo = "san jose";
+console.log(dojo);
+learn();
+console.log(dojo);
+
+//8.
+//Given:
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
+
+//After hoisting by the interpreter:
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+
+//Hoisting with ES6:
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
